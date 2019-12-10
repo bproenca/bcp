568f39b59d8d7f7d35c3e81411a08fd9f4b93d1e
Bruno Proenca <bruno.proenca@synchro.com.br>
2019-12-10 11:25:27 -0300

    sap conn

--- SecureJDBC/src/main/java/bcp/App.java
+++ SecureJDBC/src/main/java/bcp/App.java
@@ -4,16 +4,28 @@ import java.sql.*;
 import java.util.Properties;
 
 public class App {
-
     public static void main(String[] args) {
         System.out.println(System.getProperty("java.version"));
         System.out.println(System.getProperty("java.vendor"));
+        new App().run();
     }
+
+    //private String database = "vhnwa751";
+    //private String port = "30015";
+    //private String username = "SYN4TDF_DEV1";
+    //private String password = "Syn4tdf_dev_01";
+    //connectionProps.put("keyStore", trustStorePath);
+    //connectionProps.put("keyStorePassword", trustStorePassword);
+    //connectionProps.put("keyStoreType", "JKS");
+
 
     private String database = "vhs4h179";
     private String username = "SYN4TDF_DEV";
     private String password = "Syn4Tdf_Dev_01";
     private String port = "30044";
+    private String trustStorePassword = "bcp123";
+    private String trustStorePath = "/dados/wks/keystore-bcp.jks";
+
     //fieldOpcoes.setText("?autocommit=false");
 
     public void run() {
@@ -27,15 +39,16 @@ public class App {
     }
 
     private Connection getConnection() {
-        Connection connection = null;
-
-
         Properties connectionProps = new Properties();
         connectionProps.put("user", username);
         connectionProps.put("password", password);
-        //connectionProps.put("encrypt", "true");
-        //connectionProps.put("validateCertificate", "false");
+        connectionProps.put("encrypt", "true");
+        connectionProps.put("validateCertificate", "true");
+        connectionProps.put("trustStore", trustStorePath);
+        connectionProps.put("trustStorePassword", trustStorePassword);
+        connectionProps.put("trustStoreType", "JKS");
 
+        Connection connection = null;
         try {
             connection = DriverManager.getConnection(
                     "jdbc:sap://" + database + ":"+ port +"/?autocommit=false", connectionProps);
@@ -53,7 +66,14 @@ public class App {
             ResultSetMetaData rsmd = rs.getMetaData();
 
             for (int i = 1; i <= rsmd.getColumnCount(); i++) {
-                System.out.println(rsmd.getColumnName(i) + " -> " + rsmd.getColumnType(i));
+                System.out.println(rsmd.getColumnName(i) + " -> " + rsmd.getColumnTypeName(i));
+            }
+
+            while (rs.next()) {
+                for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                    System.out.print(rs.getObject(i) + " | ");
+                }
+                System.out.println();
             }
 
         } catch (SQLException e) {
